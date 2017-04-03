4bc5cae88168b51e22456e92d999d1b108bbdfe7
Morena <monteromorena@gmail.com>
2017-04-03 17:13:49 -0300

    clase 3

--- videojuego/Program.cs
+++ videojuego/Program.cs
@@ -28,12 +28,17 @@ namespace Videojuego
             int min = 0;
             int max = 50;
             int cantb = max / 4;
+            int cante = 5;
             Random random = new Random();
             int x = max / 2;
             int y = max / 2;
             int[] xb = new int[cantb];
             int[] yb = new int[cantb];
+            string[] enemigos = new string[cante];
+            int[] xe = new int[cante];
+            int[] ye = new int[cante];
             int randomNumber;
+            int movimiento;
             int i = 0;
             string[] bombas = new string[cantb];
             while (i != cantb)
@@ -46,6 +51,16 @@ namespace Videojuego
                 i++;
             }
              i = 0;
+            while (i != cante)
+            {
+                randomNumber = random.Next(min + 1, max - 1);
+                xe[i] = randomNumber;
+                randomNumber = random.Next(min + 1, max - 1);
+                ye[i] = randomNumber;
+                enemigos[i] = "A";
+                i++;
+            }
+            i = 0;
             while (i != cantb)
             {
                 if (xb[i] == x && yb[i] == y)
@@ -56,7 +71,7 @@ namespace Videojuego
                 i++;
             }
             Console.SetCursorPosition(0, y + 3);
-            Console.WriteLine("Bienvenido al Juego, use WASD para mover al personaje(☺) y presione x para salir");
+            Console.WriteLine("Bienvenido al Juego, use WASD para mover al personaje(O) y presione x para salir");
             while (end == false)
             {
 
@@ -65,6 +80,13 @@ namespace Videojuego
                 {
                     Console.SetCursorPosition(xb[i], yb[i]);
                     Console.WriteLine(bombas[i]);
+                    i++;
+                }
+                i = 0;
+                while (i != cante)
+                {
+                    Console.SetCursorPosition(xe[i], ye[i]);
+                    Console.WriteLine(enemigos[i]);
                     i++;
                 }
                 Console.SetCursorPosition(x, y);
@@ -117,7 +139,7 @@ namespace Videojuego
                         end = true;
                     }
                     i = 0;
-                    while (i != cantb)
+                    while (i != cantb|| vivo == false)
                     {
                         if (x == xb[i] && y == yb[i])
                         {
@@ -125,7 +147,40 @@ namespace Videojuego
                         }
                         i++;
                     }
-
+                    i = 0;
+                    while (i != cante)
+                    {
+                        movimiento = random.Next(0, 3);
+                        switch (movimiento)
+                        {
+                            case 0:
+                                if (xe[i] != max)
+                                xe[i] =+ 1;
+                                break;
+                            case 1:
+                                if (xe[i] != min)
+                                xe[i] =- 1;
+                                break;
+                            case 2:
+                                if (ye[i] != max)
+                                ye[i] = +1;
+                                break;
+                            case 3:
+                                if (ye[i] != min)
+                                ye[i] = -1;
+                                break;
+                        }
+                        i++;
+                    }
+                    i = 0;
+                    while (i != cante)
+                    {
+                        if (x == xe[i] && y == ye[i])
+                        {
+                            vivo = false;
+                        }
+                        i++;
+                    }
                 }
                 Console.Clear();
                 if (vivo == false)
