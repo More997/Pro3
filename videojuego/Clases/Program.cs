2d7360995ac56a1102a331124378e7d0d1c40648
More997 <moresfever@hotmail.com>
2017-05-01 15:31:54 -0300

    High Score Añadido

--- videojuego/Clases/Program.cs
+++ videojuego/Clases/Program.cs
@@ -14,6 +14,7 @@ namespace Videojuego
             FileStream fs2;
             StreamWriter sw;
             StreamReader sr;
+
             if (!File.Exists("datosJuegos.txt"))
             {
                 FileStream fs = File.Create("datosJuegos.txt");
@@ -22,12 +23,13 @@ namespace Videojuego
                 sw.WriteLine(Convert.ToString(Console.ReadLine()));
                 sw.Close();
                 fs.Close();
-
             }
+
                 fs2 = File.OpenRead("datosJuegos.txt");
                 sr = new StreamReader(fs2);
 
             Console.WriteLine("Ingrese un Nombre, por favor");
+
             string name = Console.ReadLine();
             int highscore=0;
             string tecla = "";
@@ -35,23 +37,22 @@ namespace Videojuego
             bool fin = false;
             bool newHS = false;
             FileStream HS;
-            FileStream HSname;
-            if (!File.Exists("HS.txt") && !File.Exists("Nombre.txt"))
-            {
-                HS = File.Create("HS.txt");
-                HSname = File.Create("Nombre.txt");
-            }
-            else
-            {
-                HS = File.Create("HS.txt");
-                HSname = File.Create("Nombre.txt");
-            }
-               // BinaryWriter HSw = new StreamWriter(HS);
-               //BinaryReader HSr = new StreamReader(HS);
-                StreamWriter Nw = new StreamWriter(HSname);
-                StreamReader Nr = new StreamReader(HSname);
 
-           
+            if (!File.Exists("HS.txt") )
+            {
+                HS = new FileStream("HS.txt", FileMode.Create);
+                HS.Close();
+
+            }
+            else if (File.Exists("HS.txt"))
+            {
+                BinaryReader BRHS = new BinaryReader(File.Open("HS.txt", FileMode.Open));
+                highscore = BRHS.ReadInt32();
+                nameHS = BRHS.ReadString();
+                BRHS.Close();
+            }
+            BinaryWriter BWHS = new BinaryWriter(File.Open("HS.txt", FileMode.Open));
+            
             WebRequest req = WebRequest.Create("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22buenos%20aires%2C%20tx%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
 
             WebResponse respuesta = req.GetResponse();
@@ -116,12 +117,14 @@ namespace Videojuego
                 
             if (newHS == true)
             {
-                Nw.WriteLine(Convert.ToString(name));
+                    nameHS = name;
+                    BWHS.Write(highscore);
+                    BWHS.Write(name);                 
             }
-                 
             } while (fin == false);
             sr.Close();
             fs2.Close();
+            BWHS.Close();
             Console.WriteLine("Fin del juego");
         
             
